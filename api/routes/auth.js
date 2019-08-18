cc956d1ab827d39d44bc627e9af1aad4ebdcbb3c
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-08-18 17:29:18 +0200

    auth router

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 
@@ -7,33 +7,34 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+let loginPromise = (req, user) => {
+  return new Promise((resolve, reject) => {
+    req.login(user, e => (e ? reject(e) : resolve(user)));
+  });
+};
 
-router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
-});
-
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
-
-router.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, theUser, failureDetails) => {
+    if (err) return res.status(500).json({ message: "Something went wrong" });
+    if (!theUser) return res.status(401).json(failureDetails);
+    loginPromise(req, theUser)
+      .then(() => res.status(200).json(req.user))
+      .catch(e => res.status(500).json({ message: e.message }));
+  })(req, res, next);
 });
 
 router.post("/signup", (req, res, next) => {
-  const username = req.body.username;
+  const name = req.body.name;
+  const email = req.body.email;
   const password = req.body.password;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
+  if (name === "" || email === "" || password === "") {
+    res.json({ message: "Indicate name, email and password" });
     return;
   }
 
-  User.findOne({ username }, "username", (err, user) => {
+  User.findOne({ email }, "email", (err, user) => {
     if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
+      res.json({ message: "The email already exists" });
       return;
     }
 
@@ -41,23 +42,49 @@ router.post("/signup", (req, res, next) => {
     const hashPass = bcrypt.hashSync(password, salt);
 
     const newUser = new User({
-      username,
-      password: hashPass
+      name,
+      email,
+      password: hashPass,
+      points: 0,
+      cup: false,
+      nextDay: true
     });
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+    newUser
+      .save()
+      .then(user => loginPromise(req, user))
+      .then(user => {
+        res.json({ user });
+      })
+      .catch(err => {
+        res.json({ message: "Something went wrong" });
+      });
   });
+});
+
+router.get("/currentuser", (req, res) => {
+  const { user } = req;
+  if (user) {
+    res.json({ succes: "OK", user });
+  } else {
+    res.status(401).json({ succes: "NO USER LOGGED IN" });
+  }
 });
 
 router.get("/logout", (req, res) => {
   req.logout();
-  res.redirect("/");
+  res.json({ succes: "OK" });
+});
+
+// Buscar todos los usuarios
+router.get("/allUsers", (req, res) => {
+  User.find({}, null, { sort: "-points" }, (err, users) => {
+    if (err) {
+      console.log("error query");
+    } else {
+      res.json({ users: users });
+    }
+  });
 });
 
 module.exports = router;
