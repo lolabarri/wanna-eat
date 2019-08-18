a1d8e7ced24013a385fb9f823a00f80052f011fa
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-08-18 14:01:35 +0200

    all cleared up to start again

--- client/src/lib/footballAPI.js
+++
@@ -1,5 +0,0 @@
-export const getMatchesResults = (matchday) => {
-  return fetch(`${process.env.NODE_ENV === "production" ? "":'http://localhost:8000'}/footballAPI/matches/${matchday}`).then( res => res.json()).then(data => {
-    return data;
-})
-};
